174d6e394dc5afc2a5e0b756e24c757aed17d6ad
Hashwiz.NET <info@hashwiz.net>
2021-06-20 22:28:32 +0530

    Force HTTPS

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -13,6 +13,15 @@ const { startListener } = require('./utils/listener')
 
 const app = express()
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 if (process.env.NODE_ENV !== 'test') {
   app.use(logger('dev'))
 }
