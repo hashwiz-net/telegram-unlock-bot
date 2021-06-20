80d82b4d6162e95fd2a746903fe4d4e92358ba9a
Hashwiz.NET <info@hashwiz.net>
2021-06-20 23:00:33 +0530

    revert SSL change

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -13,14 +13,14 @@ const { startListener } = require('./utils/listener')
 
 const app = express()
 
-if(process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    else
-      next()
-  })
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use((req, res, next) => {
+//     if (req.header('x-forwarded-proto') !== 'https')
+//       res.redirect(`https://${req.header('host')}${req.url}`)
+//     else
+//       next()
+//   })
+// }
 
 if (process.env.NODE_ENV !== 'test') {
   app.use(logger('dev'))
